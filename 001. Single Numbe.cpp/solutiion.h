c34f60caf0dd06219c9ec970e0db247941fe3f8f
Navie的加贝君 <rongweihe1995@126.com>
2017-07-06 00:18:22 -0500

    Update solutiion.h

--- 001. Single Numbe.cpp/solutiion.h
+++ 001. Single Numbe.cpp/solutiion.h
@@ -1,8 +1,8 @@
 class Solution {
 public:
 	int single_number(int a[],int n){
-    int r=0;
-	for(int i=0; i<n; ++i) r^=a[i];
-	return r;
+	int res=0;
+	for(int i=0; i<n; ++i) res^=a[i];
+	return res;
 	}
 };
