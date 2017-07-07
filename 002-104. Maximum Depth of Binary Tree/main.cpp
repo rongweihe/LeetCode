82c16d9dda312006520ee70c42cf936d0a19b230
Navie的加贝君 <rongweihe1995@126.com>
2017-07-07 18:31:31 +0800

    Update main.cpp

--- 002-104. Maximum Depth of Binary Tree/main.cpp
+++ 002-104. Maximum Depth of Binary Tree/main.cpp
@@ -40,27 +40,27 @@ int main()
 	  
     //freopen("in.txt","r",stdin);
     //freopen("out.txt","w",stdout);
-  TreeNode root(1);
-	TreeNode t2(2);
-	TreeNode t3(3);
-	TreeNode t4(4);
-	TreeNode t5(5);
-	TreeNode t6(6);
-	TreeNode t7(7);
-	TreeNode t8(8);
-	TreeNode t9(9);
+TreeNode root(1);
+TreeNode t2(2);
+TreeNode t3(3);
+TreeNode t4(4);
+TreeNode t5(5);
+TreeNode t6(6);
+TreeNode t7(7);
+TreeNode t8(8);
+TreeNode t9(9);
 	
-	root.left = &t2;
-	root.right = &t3;
-	t2.left = &t4;
-	t2.right = &t5;
-	t3.right = &t6;
-	t4.left = &t7;
-	t4.right = &t8;
-	t8.left = &t9;
+root.left = &t2;
+root.right = &t3;
+t2.left = &t4;
+t2.right = &t5;
+t3.right = &t6;
+t4.left = &t7;
+t4.right = &t8;
+t8.left = &t9;
 	
-	Solution s;
-	std::cout << s.maxDepth(&root) << std::endl;
+Solution s;
+std::cout << s.maxDepth(&root) << std::endl;
     
-    return 0;
+return 0;
 }
